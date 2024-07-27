f3043d9a0d1ea133b6dbb7491dfbb807a091de79
Vishnudev045 <vishnu.12100848@lpu.in>
2024-07-27 10:53:21 +0530

    added files

---
+++ data/index.ts
@@ -0,0 +1,157 @@
+export const navItems = [
+  { name: "Home", link: "#Home" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Developing an Innovative AI-Powered Online Writing Platform",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Dynamic Video Sharing App",
+    des: "Experience a feature-rich video-sharing platform built with React Native and Appwrite. This app offers seamless media uploads, engaging onboarding screens, and interactive animations, all while ensuring a responsive and enjoyable user experience across iOS and Android devices.",
+    img: "/p1.png",
+    iconLists: ["/expo.svg", "/native.svg", "/appwrite.svg", "/git.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Full-Stack Blog Application",
+    des: "Built with Node.js, Express.js, MongoDB, and EJS, this blog application features dynamic content delivery through server-side rendering. It includes secure user authentication and authorization, enabling personalized content management and full CRUD functionality. The use of MVC architecture ensures scalable and maintainable code, providing efficient data storage and retrieval for an enhanced user experience.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/MongoDB2.svg", "/expressjs2.svg", "/nodejs.svg", "/git.svg"],
+    link: "https://full-stack-mern-blog.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Hospital Management System",
+    des: "Developed a comprehensive hospital management system using React for the frontend and Node.js with Express.js for the backend, coupled with MongoDB for data storage. This full-stack application allows hospitals to create and manage their profiles, while users can search and view hospitals by city. The system features secure data handling, dynamic content updates, and an intuitive user interface for a seamless experience.",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/MongoDB2.svg", "/expressjs2.svg", "/nodejs.svg", "/git.svg"],
+    link: "https://hospital-management-frontend-livid.vercel.app/",
+  },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "/ui.apple.com",
+  // },
+];
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full-Stack Development Knowledge",
+    desc: "Proficient in building full-stack applications using React, Node.js, Express.js, and MongoDB. Capable of developing both frontend and backend components for seamless application integration and dynamic user experiences.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Android App Development",
+    desc: "Experienced in designing and developing mobile applications for Android using modern development tools and frameworks. Focused on delivering high-performance apps with a smooth user experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Frontend Development Expertise",
+    desc: "Skilled in creating dynamic and responsive user interfaces using React.js. Expert in delivering engaging and user-friendly web applications with modern frontend technologies and best practices.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Backend Development Knowledge",
+    desc: "Well-versed in backend development with Node.js and Express.js. Capable of designing robust server-side applications, handling data management with MongoDB, and ensuring secure and efficient API integration.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  }
+  
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link : "https://github.com/AkshayEddula"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Akshay__1603"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/akshayeddula/"
+  },
+];
